b728a3a0776ff6cc979595f52a1b03d1eb0e67e8
anu1701 <acharyaanush17@gmail.com>
2023-01-08 22:19:34 +0530

    Insert Products by admin

--- Admin_area/insert_product.php
+++ Admin_area/insert_product.php
@@ -18,7 +18,76 @@
     <div class="container mt-3">
         <h1 class="text-center">Insert Products</h1>
         <!-- form -->
-        
+        <form action="" method="post" enctype="multipart/form-data">
+        <!-- title -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_title" class="form-label">Product Title</label>
+                <input type="text" name="product_title" id="product_title" class="form-control" placeholder="Enter product title" autocomplete="off" required="required">
+        </div>
+
+        <!-- description -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="description" class="form-label">Product Description</label>
+                <input type="text" name="description" id="description" class="form-control" placeholder="Enter product description" autocomplete="off" required="required">
+        </div>
+
+        <!-- keywords -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_keywords" class="form-label">Product Keywords</label>
+                <input type="text" name="product_keywords" id="product_keywords" class="form-control" placeholder="Enter product keywords" autocomplete="off" required="required">
+        </div>
+
+        <!-- categories -->
+        <div class="form-outline mb-4 w-50 m-auto">
+            <select name="product_category" id="" class="form-select">
+                <option value="">Select a Category</option>
+                <option value="">Category1</option>
+                <option value="">Category2</option>
+                <option value="">Category3</option>
+                <option value="">Category4</option>
+            </select>    
+        </div>
+
+        <!-- brands -->
+        <div class="form-outline mb-4 w-50 m-auto">
+            <select name="product_brands" id="" class="form-select">
+                <option value="">Select a Brand</option>
+                <option value="">Brand1</option>
+                <option value="">Brand2</option>
+                <option value="">Brand3</option>
+                <option value="">Brand4</option>
+            </select>    
+        </div>
+
+        <!-- image 1 -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image1" class="form-label">Product image 1</label>
+                <input type="file" name="product_image1" id="product_image1" class="form-control" required="required">
+        </div>
+
+        <!-- image 2 -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image2" class="form-label">Product image 2</label>
+                <input type="file" name="product_image2" id="product_image2" class="form-control" required="required">
+        </div>
+
+        <!-- image 3 -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image3" class="form-label">Product image 3</label>
+                <input type="file" name="product_image3" id="product_image3" class="form-control" required="required">
+        </div>
+
+        <!-- price -->
+        <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_price" class="form-label">Product Price</label>
+                <input type="text" name="product_price" id="product_price" class="form-control" placeholder="Enter product price" autocomplete="off" required="required">
+        </div>
+
+        <!-- price -->
+        <div class="form-outline mb-4 w-50 m-auto">
+            <input type="submit" name="insert_product" class="btn btn-info mb-3 px-3" value="Insert Products">
+        </div>
+        </form>
     </div>
     
 </body>
