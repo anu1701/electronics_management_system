c525fa597fe08c9a9243f51baf3a99418b075d19
aanch-343 <aanchalnshetty111@gmail.com>
2023-01-16 04:05:20 +0530

    changes

--- about.php
+++ about.php
@@ -1,11 +1,23 @@
 <?php
- include('includes/header.php')  
+ include('includes/header.php') ;
 ?>
 <?php
  include('includes/connect.php')  
 ?>
  <link rel="stylesheet" href="stylesabout.css">
- hello
+ <div class="about">
+        <div class="inner-section">
+            <h1>About Us</h1>
+            <p class="text">
+            The Gadget World Is Online Store For Any Kind Of Electronic Products <br>
+            We Sell Best Products At Affordable Price In Short Time Period Our Goals Is To Provide  Best Goods And Services.
+            </p>
+            <div class="skills">
+                <button><a href="contact_us.php">
+                    Contact Us</a></button>
+            </div>
+        </div>
+    </div>
 <?php
  include('includes/footer.php')  
 ?>
