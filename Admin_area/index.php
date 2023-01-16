b738b0ebbce277edf685fd6113b091bb73e528ff
anu1701 <acharyaanush17@gmail.com>
2023-01-16 10:31:14 +0530

    Admin functions

--- Admin_area/index.php
+++ Admin_area/index.php
@@ -1,7 +1,7 @@
 <!-- connect file -->
 <?php
 include('../includes/connect.php');
-include('../functions/common_functions.php');
+// include('../functions/common_functions.php');
 ?>
 
 <!DOCTYPE html>
